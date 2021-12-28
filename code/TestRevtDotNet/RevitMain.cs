1e78fa7d3c713d280356b30d72755371c4be2f1e
zrq-github <583175160@qq.com>
2021-12-28 18:35:40 +0800

    更改命名空间

---
+++ code/TestRevtDotNet/RevitMain.cs
@@ -0,0 +1,31 @@
+﻿/*************************************************************************************
+ *
+ * 创建人员:  zrq 
+ * 创建时间:  2021/12/28 17:17:51
+ * 文件描述:  
+ * 
+*************************************************************************************/
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRevtDotNet
+{
+    [ObfuscationAttribute(Exclude = true, ApplyToMembers = true)]
+    internal class RevitMain : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
