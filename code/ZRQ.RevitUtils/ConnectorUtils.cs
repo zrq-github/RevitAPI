9e395d50e24accdcbbf66e74208b0c8af2022d73
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-03-09 17:26:53 +0800

    更新结构

---
+++ code/ZRQ.RevitUtils/ConnectorUtils.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RQ.RevitUtils
+{
+    internal class ConnectorUtils
+    {
+        /// <summary>
+        /// 获取主连接点
+        /// </summary>
+        private static Connector GetMajorConnector(FamilyInstance fit)
+        {
+            Connector mainConnector = null;
+            ConnectorSet fitConnSets = fit.MEPModel.ConnectorManager.Connectors;
+            ConnectorSetIterator iter = fitConnSets.ForwardIterator();
+            while (iter.MoveNext())
+            {
+                Connector c = iter.Current as Connector;
+                if (c.GetMEPConnectorInfo().IsPrimary)
+                {
+                    mainConnector = c;
+                    break;
+                }
+            }
+            return mainConnector;
+        }
+    }
+}
---
+++ code/ZRQ.RevitUtils/ExtensibleStorageUtils/IExtendStorageBase.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RQ.RevitUtils.ExtensibleStorageUtils
+{
+    /// <summary>
+    /// 扩展数据的基本接口
+    /// </summary>
+    public interface IExtendStorageBase : IUpdateStorage
+    {
+    }
+}
