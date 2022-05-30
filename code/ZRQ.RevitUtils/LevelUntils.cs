783efa3eb61723ae0a492ef952329420c5c7b6ac
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-05-30 18:19:42 +0800

    Create LevelUntils.cs

---
+++ code/ZRQ.RevitUtils/LevelUntils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRQ.RevitUtils
+{
+    internal static class LevelUntils
+    {
+        public static List<Level> GetAllLevels(Document doc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Levels);
+            return collector.ToElements().ToList().ConvertAll(p => p as Level);
+        }
+    }
+}
