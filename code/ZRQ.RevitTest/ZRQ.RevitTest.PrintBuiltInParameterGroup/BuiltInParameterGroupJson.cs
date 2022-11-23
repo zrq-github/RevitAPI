cf509b3846ac33b9643ddb734133e1da3aca7480
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-11-23 09:59:40 +0800

    ZRQ.RevitTest.PrintBuiltInParameterGroup

---
+++ code/ZRQ.RevitTest/ZRQ.RevitTest.PrintBuiltInParameterGroup/BuiltInParameterGroupJson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRQ.RevitTest.PrintBuiltInParameterGroup
+{
+    public class BuiltInParameterGroupJson
+    {
+        /// <summary>
+        /// 参数分组的名字
+        /// </summary>
+        public string ParameterGroupName { get; set; }
+
+        /// <summary>
+        /// 对应的枚举值
+        /// </summary>
+        public int ParameterValue { get; set; }
+
+        /// <summary>
+        /// 枚举名字
+        /// </summary>
+        public string EnumName { get; set; }
+    }
+}
---
+++ code/ZRQ.RevitTest/ZRQ.RevitTest.PrintBuiltInParameterGroup/ExternalCommondSample.cs
@@ -0,0 +1,54 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZRQ.RevitTest.PrintBuiltInParameterGroup;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ZRQ.RevitAPITest.Sample
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class ExternalCommondSample : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            List<BuiltInParameterGroupJson> builtInParameterGroupJsons = new();
+            var enums = Enum.GetValues(typeof(BuiltInParameterGroup));
+            foreach (BuiltInParameterGroup strEnum in enums)
+            {
+                BuiltInParameterGroupJson group = new();
+                string name = LabelUtils.GetLabelFor(strEnum);
+                string enumName = strEnum.ToString();
+                int parameterValue = (int)strEnum;
+
+                group.ParameterGroupName = name;
+                group.EnumName = enumName;
+                group.ParameterValue = parameterValue;
+                builtInParameterGroupJsons.Add(group);
+            }
+
+            var json = JsonConvert.SerializeObject(builtInParameterGroupJsons);
+
+            writeJsonFile(@"C:\Users\zrq\Desktop\xt_v9.2.0\builtInParameterGroupJsons.json", json);
+
+            //将序列化的json字符串内容写入Json文件，并且保存
+            void writeJsonFile(string path, string jsonConents)
+            {
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, System.IO.FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                    {
+                        sw.WriteLine(jsonConents);
+                    }
+                }
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
