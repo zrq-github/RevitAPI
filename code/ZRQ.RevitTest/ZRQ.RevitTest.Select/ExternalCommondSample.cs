e69149e78e0f2be783eb9fd08c66c8c48e22a507
zrq-github <46354863+zrq-github@users.noreply.github.com>
2023-01-12 10:46:00 +0800

    测试revit选择

---
+++ code/ZRQ.RevitTest/ZRQ.RevitTest.Select/ExternalCommondSample.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRQ.RevitTest.Select
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class ExternalCommondSample : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            Selection select = uiApp.ActiveUIDocument.Selection;
+
+            #region
+            // select.PickObjects ObjectType.Element
+            List<Reference> listRefers = select.PickObjects(ObjectType.Element).ToList();
+            #endregion
+
+            return Result.Succeeded;
+        }
+    }
+}
